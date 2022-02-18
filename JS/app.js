a546c8b53ee88e9bbd6fcd6bba8178450d4e38fa
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-02-18 21:12:55 +0600

    js code

---
+++ JS/app.js
@@ -0,0 +1,4 @@
+/* ============ Get All Variables ============ */
+
+const deposit_Display = document.getElementById("deposit-display")
+const withdraw_Display = document.getElementById("withdraw-display")
