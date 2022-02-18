c430624d503979bdc8ee5d4f0a7b66b5b2acbe93
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-02-18 23:05:36 +0600

    Deposti Button Added

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,30 @@
-/* ============ Get All Variables ============ */
+/* =========== Common Function =========== */
+function getInteger(input, text){
+    let inputResult;
+    const getInput = document.getElementById(input);
+    if(text){
+        inputResult = parseInt(getInput.innerText)
+    }else{
+        inputResult = parseInt(getInput.value)
+    }
 
-const deposit_Display = document.getElementById("deposit-display")
-const withdraw_Display = document.getElementById("withdraw-display")
+    /* Emptify Input */
+    getInput.value = ""
+    return inputResult;
+}
+
+function updateBal(input){
+    let balance_Dis = getInteger("balance-display", true)
+    let result = balance_Dis + input
+    document.getElementById("balance-display").innerText = result;
+}
+
+/* =========== Deposit Button =========== */
+document.getElementById("deposit-btn").addEventListener("click", () => {
+    let deposit_Dis = getInteger("deposit-display", true);
+    let deposit_Int = getInteger("deposit-input", false);
+    let result = deposit_Dis + deposit_Int
+
+    document.getElementById("deposit-display").innerText = result
+    updateBal(result)
+})
